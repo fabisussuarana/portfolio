7290362afa4dbada35ddaec48b8e0fd51794235d
fabisussuarana <bianasussuaranadasilva17@gmail.com>
2024-05-24 06:33:10 -0500

    Iniciando portfolio

---
+++ portfolio-site/src/Components/Body/Body.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Body.css';
+
+const Body = () => {
+  return (
+    <div>
+      {/* home */}
+      <section className='home-section'>
+
+      </section>
+    </div>
+  )
+}
+
+export default Body;
